4f90330ce1ef0dc8b6cf299da9a196c20e5ef8ee
glennswest <gwest@redhat.com>
2019-05-25 17:11:55 -0500

    Make sure the start_kube script is copied

--- getkube.sh
+++ getkube.sh
@@ -5,17 +5,19 @@ mkdir wip
 mkdir wip/bin
 mkdir wip/bin/metadata
 export VERSION=v1.11.3
+export COMPNAME=kube
 export wcontent=${GOPATH}/src/github.com/glennswest/wcontent
-rm $GOPATH/src/github.com/glennswest/wcontent/content/kube_$VERSION.ign
-curl -L -k https://dl.k8s.io/$VERSION/kubernetes-node-windows-amd64.tar.gz -o /tmp/kubernetes.tar.gz
+rm $GOPATH/src/github.com/glennswest/wcontent/content/${COMPNAME}_${VERSION}.ign
+curl -L -k https://dl.k8s.io/${VERSION}/kubernetes-node-windows-amd64.tar.gz -o /tmp/kubernetes.tar.gz
 (cd src;tar xvzf /tmp/kubernetes.tar.gz )
 cp src/kubernetes/node/bin/* wip/bin
-(cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ${wcontent}/content/kube_$VERSION.ign bin)
-cp metadata/kube_$VERSION.metadata wip/bin/metadata/kube_$VERSION.metadata
-(cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool um ${wcontent}/content/kube_$VERSION.ign bin/metadata/kube_$VERSION.metadata)
-(cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ${wcontent}/content/kube_$VERSION.ign bin/metadata)
-$GOPATH/src/github.com/glennswest/libignition/igntool/igntool ls ${wcontent}/content/kube_$VERSION.ign
-cat wip/bin/metadata/kube_$VERSION.metadata
+cp files/${COMPNAME}_${VERSION}/bin/* wip/bin
+(cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ${wcontent}/content/${COMPNAME}_$VERSION.ign bin)
+cp metadata/${COMPNAME}_${VERSION}.metadata wip/bin/metadata/${COMPNAME}_${VERSION}.metadata
+(cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool um ${wcontent}/content/${COMPNAME}_${VERSION}.ign bin/metadata/${COMPNAME}_${VERSION}.metadata)
+(cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ${wcontent}/content/kube_${VERSION}.ign bin/metadata)
+$GOPATH/src/github.com/glennswest/libignition/igntool/igntool ls ${wcontent}/content/${COMPNAME}_${VERSION}.ign
+cat wip/bin/metadata/${COMPNAME}_${VERSION}.metadata
 rm -r -f wip/*
 rm -r -f src
 
