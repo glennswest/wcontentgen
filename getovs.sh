4419ffdb74cd2a04641060ca05522fc1354e65ed
glennswest <gwest@redhat.com>
2019-05-25 14:11:46 -0500

    Fix data directory

--- getovs.sh
+++ getovs.sh
@@ -1,6 +1,8 @@
 rm -r -f wip
 mkdir wip
 mkdir wip/bin
+mkdir wip/k
+mkdir wip/k/data
 mkdir wip/bin/metadata
 export VERSION=2.70beta
 export COMPNAME=ovs
@@ -10,6 +12,7 @@ echo $COMPNAME
 rm ${wcontent}/content/${COMPNAME}_${VERSION}.ign
 cp metadata/${COMPNAME}_${VERSION}.metadata wip/bin/metadata
 cp files/${COMPNAME}_${VERSION}/bin/*.ps1 wip/bin
+cp files/${COMPNAME}_${VERSION}/k/data/* wip/k/data/
 wget https://cloudbase.it/downloads/openvswitch-hyperv-installer-beta.msi -O wip/bin/openvswitch-hyperv-installer-beta.msi
 (cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ${wcontent}/content/${COMPNAME}_${VERSION}.ign bin)
 (cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ${wcontent}/content/${COMPNAME}_${VERSION}.ign k)
