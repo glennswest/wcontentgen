f7fcb7a55c739346cd2c82f72bc39523ee182762
glennswest <gwest@redhat.com>
2019-05-25 17:30:17 -0500

    Make sure the setup script is copied for sdn component

--- getsdn.sh
+++ getsdn.sh
@@ -4,6 +4,7 @@ mkdir wip/bin/metadata
 export VERSION=v1.0.1
 export COMPNAME=sdn
 export wcontent=${GOPATH}/src/github.com/glennswest/wcontent
+cp files/${COMPNAME}_${VERSION}/bin/*.ps1 wip/bin
 (cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ${wcontent}/content/${COMPNAME}_${VERSION}.ign bin)
 cp metadata/${COMPNAME}_$VERSION.metadata wip/bin/metadata/${COMPNAME}_${VERSION}.metadata
 (cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool um ${wcontent}/content/${COMPNAME}_$VERSION.ign bin/metadata/${COMPNAME}_${VERSION}.metadata)
