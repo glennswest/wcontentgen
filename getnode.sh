06ad8b4e33455a19f6f1f8cee5f9cad937a5a4a1
glennswest <gwest@redhat.com>
2019-05-25 11:52:28 -0500

    Make sure k directory is created

--- getnode.sh
+++ getnode.sh
@@ -1,10 +1,13 @@
 rm -r -f wip/bin
 mkdir wip/bin
 mkdir wip/bin/metadata
+mkdir wip/k
+echo "Kubernetes config Dir" > wip/k/readme.txt
 export VERSION=1.0.2
 export COMPNAME=node
 export wcontent=${GOPATH}/src/github.com/glennswest/wcontent
 (cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ${wcontent}/content/${COMPNAME}_${VERSION}.ign bin)
+(cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ${wcontent}/content/${COMPNAME}_${VERSION}.ign k)
 cp metadata/${COMPNAME}_$VERSION.metadata wip/bin/metadata/${COMPNAME}_${VERSION}.metadata
 (cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool um ${wcontent}/content/${COMPNAME}_$VERSION.ign bin/metadata/${COMPNAME}_${VERSION}.metadata)
 (cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ${wcontent}/content/${COMPNAME}_$VERSION.ign bin/metadata)
