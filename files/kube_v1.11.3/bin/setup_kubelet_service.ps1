954d40a5fbea106d32578b9593efa74327b2122f
glennswest <gwest@redhat.com>
2019-05-28 15:13:54 -0500

    compoent kube: Fix path for kubelet start

--- files/kube_v1.11.3/bin/setup_kubelet_service.ps1
+++ files/kube_v1.11.3/bin/setup_kubelet_service.ps1
@@ -1,9 +1,9 @@
 $powershell = (Get-Command powershell).Source
-$scriptPath = "C:\k\hybrid\3.11\bin\start_kubelet.ps1"
+$scriptPath = "C:\bin\start_kubelet.ps1"
 $arguments = '-ExecutionPolicy Bypass -NoProfile -File "{0}"' -f $scriptPath
-$serviceName = "ocpkubelet"
+$serviceName = "kubelet-ocp"
 nssm install $serviceName $powershell $arguments
-nssm set ocpkubelet DisplayName ocpkubelet
+nssm set ocpkubelet DisplayName kubelet-ocp
 nssm set ocpkubelet Description Kubelet For OpenShift
 nssm set ocpkubelet Start SERVICE_AUTO_START
 nssm set ocpkubelet AppStdout C:\k\logs\kubelet.log
