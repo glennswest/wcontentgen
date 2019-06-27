92a95582348d0daebba14238e074b1a55092daf0
gwest <gwest@redhat.com>
2019-06-28 03:26:31 +0800

    Add templates from wcontent to data container

--- createimage.sh
+++ createimage.sh
@@ -3,6 +3,6 @@ docker rm winoperatordata
 docker create --name winoperatordata busybox 
 docker commit winoperatordata glennswest/winoperatordata:latest
 docker rm winoperatordata
-docker run -d --name winoperatordata glennswest/winoperatordata:latest sh -c 'mkdir /content;sleep infinity'  
+docker run -d --name winoperatordata glennswest/winoperatordata:latest sh -c 'mkdir /templates;mkdir /content;sleep infinity'  
 
 
--- mvtocontainer.sh
+++ mvtocontainer.sh
@@ -1,10 +1,10 @@
 export wdir=`pwd`
 export wcontent=${GOPATH}/src/github.com/glennswest/winoperatordata/wcontent
+cp templates/* ${wcontent}
 cd ${wcontent}
 tar cvzf ../wip.taz *
 cd $wdir
 docker cp wip.taz  winoperatordata:/content
-echo "Exec the untar"
 docker exec winoperatordata  sh -c "cd /content;tar xvzf wip.taz"
 docker exec winoperatordata  sh -c "cd /content;rm wip.taz"
 docker exec winoperatordata  sh -c "cd /content;ls -l -R -h"
