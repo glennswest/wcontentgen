2b2d3dda9dddc6c234f3575e4ac8097306e885fe
gwest <gwest@redhat.com>
2019-06-28 05:27:04 +0800

    Add kube_v1.13.6 online compoonent

--- getkube_offline.sh
+++ getkube_offline.sh
@@ -7,15 +7,14 @@ mkdir wip/bin/metadata
 mkdir wip/k
 mkdir wip/k/logs
 touch wip/k/logs/kubelet.log
-export VERSION=v1.13.6
+export VERSION=v1.11.3
 export COMPNAME=kube
 export wcontent=${GOPATH}/src/github.com/glennswest/winoperatordata/wcontent
 rm $GOPATH/src/github.com/glennswest/wcontent/content/${COMPNAME}_${VERSION}.ign
-export KUBEURL=https://dl.k8s.io/${VERSION}/kubernetes-node-windows-amd64.tar.gz 
+curl -L -k https://dl.k8s.io/${VERSION}/kubernetes-node-windows-amd64.tar.gz -o /tmp/kubernetes.tar.gz
 (cd src;tar xvzf /tmp/kubernetes.tar.gz )
 cp src/kubernetes/node/bin/* wip/bin
 cp files/${COMPNAME}_${VERSION}/bin/* wip/bin
-(cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool ar ${wcontent}/content/${COMPNAME}_$VERSION.ign ${KUBEURL} /k/tmp/kubernets.tar.gz )
 (cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ${wcontent}/content/${COMPNAME}_$VERSION.ign bin)
 (cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ${wcontent}/content/${COMPNAME}_$VERSION.ign k)
 cp metadata/${COMPNAME}_${VERSION}.metadata wip/bin/metadata/${COMPNAME}_${VERSION}.metadata
