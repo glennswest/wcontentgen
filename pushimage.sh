2318466d11315fc66ab77128d3d6fa4aac81d826
gwest <gwest@redhat.com>
2019-06-28 03:30:06 +0800

    Make sure we tag latest on data container

--- pushimage.sh
+++ pushimage.sh
@@ -2,6 +2,7 @@ export GIT_COMMIT=$(git rev-parse --short HEAD)
 docker commit winoperatordata glennswest/winoperatordata:latest
 docker tag glennswest/winoperatordata:latest  glennswest/winoperatordata:$GIT_COMMIT
 docker tag glennswest/winoperatordata:$GIT_COMMIT  docker.io/glennswest/winoperatordata:$GIT_COMMIT
+docker tag docker.io/glennswest/winoperatordata:$GIT_COMMIT docker.io/glennswest/winoperatordata:latest
 docker push docker.io/glennswest/winoperatordata:$GIT_COMMIT
 
 
