47bcd019f822ab5b86b1cdc919257903c04c5b5d
gwest <gwest@redhat.com>
2019-06-28 21:02:55 +0800

    Fix container name

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ git push origin master
 export GIT_COMMIT=$(git rev-parse --short HEAD)
 rm -r -f tmp
 mkdir tmp
-docker build --no-cache -t glennswest/winmachineman:$GIT_COMMIT .
-docker tag glennswest/winmachineman:$GIT_COMMIT  docker.io/glennswest/winmachineman:$GIT_COMMIT
-docker push docker.io/glennswest/winmachineman:$GIT_COMMIT
+docker build --no-cache -t glennswest/winoperatordata:$GIT_COMMIT .
+docker tag glennswest/winoperatordata:$GIT_COMMIT  docker.io/glennswest/winoperatordata:$GIT_COMMIT
+docker push docker.io/glennswest/winoperatordata:$GIT_COMMIT
