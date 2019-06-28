693c962750b4a9f0af052c07bfc2189ec1ed6abc
gwest <gwest@redhat.com>
2019-06-28 20:04:45 +0800

    Update online kube component

--- getkube.sh
+++ getkube.sh
@@ -1,6 +1,4 @@
 rm -r -f wip
-rm -r -f src
-mkdir src
 mkdir wip
 mkdir wip/bin
 mkdir wip/bin/metadata
@@ -10,11 +8,9 @@ touch wip/k/logs/kubelet.log
 export VERSION=v1.13.6
 export COMPNAME=kube
 export wcontent=${GOPATH}/src/github.com/glennswest/winoperatordata/wcontent
-rm $GOPATH/src/github.com/glennswest/wcontent/content/${COMPNAME}_${VERSION}.ign
 export KUBEURL=https://dl.k8s.io/${VERSION}/kubernetes-node-windows-amd64.tar.gz 
-(cd src;tar xvzf /tmp/kubernetes.tar.gz )
-cp src/kubernetes/node/bin/* wip/bin
 cp files/${COMPNAME}_${VERSION}/bin/* wip/bin
+rm ${wcontent}/content/${COMPNAME}_$VERSION.ign
 (cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool ar ${wcontent}/content/${COMPNAME}_$VERSION.ign ${KUBEURL} /k/tmp/kubernets.tar.gz )
 (cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ${wcontent}/content/${COMPNAME}_$VERSION.ign bin)
 (cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ${wcontent}/content/${COMPNAME}_$VERSION.ign k)
