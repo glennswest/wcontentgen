a950473b3b92d7c970e18fcd3e955f61e60660b0
gwest <gwest@redhat.com>
2019-06-30 20:05:56 +0800

    Fix ovn component naming issue

--- files/ovn_16e1a3cf/bin/genovnconf.ps1
+++
@@ -1,9 +0,0 @@
-sleep 5
-$Env:hosttoken = [IO.File]::ReadAllText("C:\Program Files\WIndowsNodeManager\data\ovn.key")
-$Env:apiserver="https://${Env:master}:8443"
-$Env:ovnnorth="tcp://${Env:masterip}:6641"
-$Env:ovnsouth="tcp://${Env:masterip}:6642"
-cd \cni
-Get-Content ovn_k8s.conf.template | ForEach-Object { $ExecutionContext.InvokeCommand.ExpandString($_) } > ovn_k8s.conf.x
-Get-Content .\ovn_k8s.conf.x | set-Content -Encoding Ascii ovn_k8s.conf
-del .\ovn_k8s.conf.x
