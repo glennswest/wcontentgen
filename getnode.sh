8b83c90fbf34d6a87c1e87d2516b4e70e2d73365
glennswest <gwest@redhat.com>
2019-07-24 15:13:58 -0500

    cleanup node and oc component

--- getnode.sh
+++ getnode.sh
@@ -10,11 +10,11 @@ export VERSION=2.0.0
 export COMPNAME=node
 cp files/${COMPNAME}_${VERSION}/etc/kubernetes/* wip/etc/kubernetes
 cp files/${COMPNAME}_${VERSION}/bin/* wip/bin
-(cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a wcontent/content/${COMPNAME}_${VERSION}.ign etc/kubernetes)
-(cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a wcontent/content/${COMPNAME}_${VERSION}.ign bin)
-(cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a wcontent/content/${COMPNAME}_${VERSION}.ign k)
+(cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ../wcontent/content/${COMPNAME}_${VERSION}.ign etc/kubernetes)
+(cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ../wcontent/content/${COMPNAME}_${VERSION}.ign bin)
+(cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ../wcontent/content/${COMPNAME}_${VERSION}.ign k)
 cp metadata/${COMPNAME}_$VERSION.metadata wip/bin/metadata/${COMPNAME}_${VERSION}.metadata
-(cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool um wcontent/content/${COMPNAME}_$VERSION.ign bin/metadata/${COMPNAME}_${VERSION}.metadata)
-(cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a wcontent/content/${COMPNAME}_$VERSION.ign bin/metadata)
+(cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool um ../wcontent/content/${COMPNAME}_$VERSION.ign bin/metadata/${COMPNAME}_${VERSION}.metadata)
+(cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ../wcontent/content/${COMPNAME}_$VERSION.ign bin/metadata)
 $GOPATH/src/github.com/glennswest/libignition/igntool/igntool ls wcontent/content/${COMPNAME}_${VERSION}.ign
 ./mvtocontainer.sh
