0a5e009174d4552b9caee11f7268534499c1103d
glennswest <gwest@redhat.com>
2019-07-29 15:02:09 -0500

    Fix type-o in genovnkey.ps1 for ovn component

--- files/ovn_16e1a3cf/bin/genovnkey.ps1
+++ files/ovn_16e1a3cf/bin/genovnkey.ps1
@@ -1,4 +1,4 @@
 oc create serviceaccount ovn
 oc adm policy add-scc-to-user privileged -z ovn
 oc adm policy add-cluster-role-to-user cluster-admin -z ovn
-oc sa get-token ovn > C:\Program Files\WIndowsNodeManager\data\ovn.key
+oc sa get-token ovn > C:\Program Files\WindowsNodeManager\data\ovn.key
