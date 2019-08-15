8f43a82e650a9245495b50b39f1cbf944744a910
glennswest <gwest@redhat.com>
2019-08-15 18:51:25 -0500

    Make sure the data folder exists for ovn key storage in ovn component

--- files/ovn_16e1a3cf/bin/genovnkey.ps1
+++ files/ovn_16e1a3cf/bin/genovnkey.ps1
@@ -1,4 +1,5 @@
 oc create serviceaccount ovn
 oc adm policy add-scc-to-user privileged -z ovn
 oc adm policy add-cluster-role-to-user cluster-admin -z ovn
+New-Item -path "C:\Program Files\WindowsNodeManager\data" -type directory
 oc sa get-token ovn | Out-File  C:\Program Files\WindowsNodeManager\data\ovn.key
