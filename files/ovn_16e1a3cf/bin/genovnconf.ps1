475d082420951b54e196b1cb0d958fe4d76f31b8
glennswest <gwest@redhat.com>
2019-08-15 11:31:18 -0500

    Fix typo in ovn conf generation for ovn component

--- files/ovn_16e1a3cf/bin/genovnconf.ps1
+++ files/ovn_16e1a3cf/bin/genovnconf.ps1
@@ -1,5 +1,5 @@
 sleep 5
-$Env:hosttoken = [IO.File]::ReadAllText("C:\Program Files\WIndowsNodeManager\data\ovn.key")
+$Env:hosttoken = [IO.File]::ReadAllText("C:\Program Files\WindowsNodeManager\data\ovn.key")
 $Env:apiserver="https://${Env:master}:8443"
 $Env:ovnnorth="tcp://${Env:masterip}:6641"
 $Env:ovnsouth="tcp://${Env:masterip}:6642"
