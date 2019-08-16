58dd5503c247e40b321f06aff9c2b5753e902bf4
glennswest <gwest@redhat.com>
2019-08-15 21:33:05 -0500

    Fix missing flag for ovn component

--- files/ovn_16e1a3cf/bin/genovnkey.ps1
+++ files/ovn_16e1a3cf/bin/genovnkey.ps1
@@ -2,4 +2,4 @@ oc create serviceaccount ovn
 oc adm policy add-scc-to-user privileged -z ovn
 oc adm policy add-cluster-role-to-user cluster-admin -z ovn
 New-Item -path "C:\Program Files\WindowsNodeManager\data" -type directory
-oc sa get-token ovn | Out-File  C:\Program Files\WindowsNodeManager\data\ovn.key
+oc sa get-token ovn | Out-File -FilePath "C:\Program Files\WindowsNodeManager\data\ovn.key"
