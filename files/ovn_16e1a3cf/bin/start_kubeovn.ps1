a8157d871051a24b912829a18d90d37fe81ff614
glennswest <gwest@redhat.com>
2019-08-20 08:51:33 -0500

    Use the .kube config for auth on ovn component

--- files/ovn_16e1a3cf/bin/start_kubeovn.ps1
+++ files/ovn_16e1a3cf/bin/start_kubeovn.ps1
@@ -1,5 +1,7 @@
 C:\bin\ovnkube.exe --init-node $env:COMPUTERNAME.ToLower() `
+                   -k8s-kubeconfig="C:\Users\Administrator\.kube\config" `
                    --config-file "C:\cni\ovn_k8s.conf" `
                    -cluster-subnet 10.128.0.0/14 `
                    -cni-conf-dir="C:\cni" `
                    -service-cluster-ip-range 172.30.0.0/16
+
