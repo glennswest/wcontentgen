a5c9b4f75a1ca3fd546102c812578165f7e08363
glennswest <gwest@redhat.com>
2019-08-21 12:05:28 -0500

    Fix ovn ip address to be dynamic based on ovn-master pod for ovn component

--- files/ovn_16e1a3cf/bin/genovnconf.ps1
+++ files/ovn_16e1a3cf/bin/genovnconf.ps1
@@ -1,9 +1,11 @@
 sleep 5
 $Env:hosttoken = [IO.File]::ReadAllText("C:\Program Files\WindowsNodeManager\data\ovn.key")
+$masterpod = (oc project openshift-ovn-kubernetes | oc get pods | Select-String -Pattern  master | % {$_ -replace("`r`n","`n")} | %{ $_-split(" ") } |select-object -first 1)
+$OVNHOSTID=(oc get pod/$masterpod -o=go-template='{{ .status.hostIP }}')
 $Env:hosttoken = $Env:hosttoken -replace "`t|`n|`r",""
-$Env:apiserver="https://${Env:master}:6443"
-$Env:ovnnorth="tcp://${Env:masterip}:6641"
-$Env:ovnsouth="tcp://${Env:masterip}:6642"
+$Env:apiserver="https://${OVNHOSTID}:6443"
+$Env:ovnnorth="tcp://${OVNHOSTID}:6641"
+$Env:ovnsouth="tcp://${OVNHOSTID}:6642"
 cd \cni
 Get-Content ovn_k8s.conf.template | ForEach-Object { $ExecutionContext.InvokeCommand.ExpandString($_) } > ovn_k8s.conf.x
 Get-Content .\ovn_k8s.conf.x | set-Content -Encoding Ascii ovn_k8s.conf
