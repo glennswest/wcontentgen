0c6c79e7709f054a2c829fd9c29e9c5726e428eb
glennswest <gwest@redhat.com>
2019-08-21 09:05:41 -0500

    Add more time as ovsdb not ready

--- files/ovs_2.70beta/bin/ovs-setup.ps1
+++ files/ovs_2.70beta/bin/ovs-setup.ps1
@@ -7,7 +7,7 @@ if($env:item -notin $systemPath) {
   }
 Set-Service -Name ovsdb-server -StartupType Automatic
 Start-Service ovsdb-server
-sleep 5
+sleep 10
 $GUID = (New-Guid).Guid; Write-Output $GUID
 ovs-vsctl --db="unix:C:/ProgramData/openvswitch/db.sock" set Open_vSwitch . external_ids:system-id=$guid
 
