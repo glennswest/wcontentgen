8a2ae22cbe1dcb9585f5dd7c3726738c78fbbf5c
glennswest <gwest@redhat.com>
2019-08-23 21:05:09 -0500

    Add support for upstream ovn

---
+++ files/ovn_03208877/bin/genovnkey.ps1
@@ -0,0 +1,5 @@
+oc create serviceaccount ovn
+oc adm policy add-scc-to-user privileged -z ovn
+oc adm policy add-cluster-role-to-user cluster-admin -z ovn
+New-Item -path "C:\Program Files\WindowsNodeManager\data" -type directory
+oc sa get-token ovn | Out-File -FilePath "C:\Program Files\WindowsNodeManager\data\ovn.key"
