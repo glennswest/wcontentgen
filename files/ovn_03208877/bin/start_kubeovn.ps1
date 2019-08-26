375685df34f009d6f319b86cb91a178fe28f9400
glennswest <gwest@redhat.com>
2019-08-26 10:39:47 -0500

    Make start_kubeovn.ps1 match 4.2 run command for ovn componet

--- files/ovn_03208877/bin/start_kubeovn.ps1
+++ files/ovn_03208877/bin/start_kubeovn.ps1
@@ -1,7 +1,12 @@
 C:\bin\ovnkube.exe --init-node $env:COMPUTERNAME.ToLower() `
                    -k8s-kubeconfig="C:\Users\Administrator\.kube\config" `
                    --config-file "C:\cni\ovn_k8s.conf" `
-                   -cluster-subnet 10.128.0.0/14 `
+                   -cluster-subnet 10.128.0.0/14/23 `
                    -cni-conf-dir="C:\cni" `
-                   -service-cluster-ip-range 172.30.0.0/16
+                   --nodeport `
+                   --k8s-service-cidr=172.30.0.0/16 `
+                   --loglevel=4 `
+                   --gateway-mode=local
+
+
 
